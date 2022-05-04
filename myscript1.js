0df0b7b66daf384a1993b9fabddc1109c2ac4b56
ZhouPengke <102636939+ZhouPengke@users.noreply.github.com>
2022-05-04 15:33:37 +0800

    Update myscript1.js

--- myscript1.js
+++ myscript1.js
@@ -1,7 +1,7 @@
 text = prompt ("Are you a student or a parent? Pleace enter student or parent")
 if (text === "student") {
- window.location.href = "https://fangjingbin.github.io/5.3/course.html";
+ window.location.href = "https://zhoupengke.github.io/zhou41199/Course.html";
 }else if (text === "parent"){
-window.location.href = "https://fangjingbin.github.io/5.3/index.html";
+window.location.href = "https://zhoupengke.github.io/zhou41199/home.html";
 }
 document.getElsementById("demo").innerHTML =" end script3";
