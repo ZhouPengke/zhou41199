176ba9af481a98102bff5494e456282b9a1f7818
ZhouPengke <102636939+ZhouPengke@users.noreply.github.com>
2023-03-12 14:44:36 +0800

    Delete myscript1.js

--- myscript1.js
+++
@@ -1,7 +0,0 @@
-text = prompt ("Are you a student or a parent? Pleace enter student or parent")
-if (text === "student") {
- window.location.href = "https://zhoupengke.github.io/zhou41199/Course.html";
-}else if (text === "parent"){
-window.location.href = "https://zhoupengke.github.io/zhou41199/home.html";
-}
-document.getElsementById("demo").innerHTML =" end script3";
